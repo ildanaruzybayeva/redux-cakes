c53c70e32daea200dca697894055e48fc0aad698
Ildana <ildana.ruzybayeva@gmail.com>
2020-10-09 15:13:31 +0000

    Added redux-dev-tool

--- cakeApp/src/redux/store.js
+++ cakeApp/src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from 'redux-logger'
 import cakeReducer from "../redux/cake/cakeReducer";
 import iceCreamReducer from "../redux/icecream/iceCreamReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(logger)));
 console.log("Initial state is", store.getState());
 
 export default store;

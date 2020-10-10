c156590f3ac0f3c2a7d483a7f7cfbf1cfb139d67
Ildana <ildana.ruzybayeva@gmail.com>
2020-10-10 21:25:50 +0000

    Action payload to buy custom amount of cakes

--- cakeApp/src/components/CakeContainer.js
+++ cakeApp/src/components/CakeContainer.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 import { buyCake } from "../redux";
 
 function CakeContainer(props) {
+    const [num, setNum] = useState(1)
   return (
     <div>
       <h2>Number of cakes {props.numOfCakes}</h2>
-      <button onClick={props.buyCake}>Buy a cake</button>
+      <input type="text" value={num} onChange={(e)=> setNum(e.target.value)}/> 
+      <button onClick={() => props.buyCake(num)}>Buy {num} cake</button>
     </div>
   );
 }
@@ -19,7 +21,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    buyCake: () => dispatch(buyCake())
+    buyCake: (num) => dispatch(buyCake(num))
   };
 }; //useDispatch - hooks alternative
 
